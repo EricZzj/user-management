33cf007918cf9c4ae251a44869f04e2f03a48bb6
张健 <zhangjian@Eric-Macbook-Pro.local>
2021-02-19 17:15:47 +0800

    init

---
+++ user-management-api/src/main/java/com/eric/usermanagement/usermanagement/api/UserService.java
@@ -0,0 +1,14 @@
+package com.eric.usermanagement.usermanagement.api;
+
+
+import com.eric.usermanagement.usermanagement.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
