1358db9e2bf135ce6c82176bcc4ce53e06c49453
张健 <zhangjian@Eric-Macbook-Pro.local>
2021-02-20 15:27:01 +0800

    1

--- user-management-web/src/main/java/com/eric/usermanagement/usermanagement/web/UserManagementApplication.java
+++ user-management-web/src/main/java/com/eric/usermanagement/usermanagement/web/UserManagementApplication.java
@@ -3,17 +3,11 @@ package com.eric.usermanagement.usermanagement.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(
-        scanBasePackages = {
-                "com.eric.usermanagement.usermanagement.*",
-        })
-//@ComponentScan("com.eric.usermanagement.usermanagement.dao.*")
-//@ComponentScan("***.***") // 1. 多模块项目需要扫描的包
-@EnableJpaRepositories("com.eric.usermanagement.usermanagement.dao.interfaces") // 2. Dao 层所在的包
-@EntityScan("com.eric.usermanagement.usermanagement.dao.dataobject") // 3. Entity 所在的包
+@SpringBootApplication(scanBasePackages = {"com.eric.usermanagement.usermanagement.*"})
+@EnableJpaRepositories("com.eric.usermanagement.usermanagement.dao.interfaces")
+@EntityScan("com.eric.usermanagement.usermanagement.dao.dataobject")
 public class UserManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserManagementApplication.class, args);
